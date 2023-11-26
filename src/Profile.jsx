56748f6c5a1742a20d4e75a6e15aaabc0f59d275
naveenterance <naveenterance@gmail.com>
2023-11-26 18:03:34 +0530

    profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,28 +1,6 @@
 import React, { useState, useEffect } from "react";
-import SmartSlider from "react-smart-slider";
-const Profile = () => {
-  const slidesArray = [
-    {
-      url: "pokemon-full.jpg",
 
-      // (Optional) Set if you want to add any content on your slide
-    },
-    {
-      url: "https://naveenterance.github.io/cv-old/images/code.jpg",
-    },
-    {
-      url: "https://naveenterance.github.io/cv-old/images/pdo.jpg",
-    },
-    {
-      url: "https://naveenterance.github.io/cv-old/images/django.jpg",
-    },
-    {
-      url: "https://naveenterance.github.io/cv-old/images/threshold.jpg",
-    },
-    {
-      url: "https://naveenterance.github.io/cv-old/images/Timed_form.jpg",
-    },
-  ];
+const Profile = () => {
   return (
     <>
       <div className=" my-2  min-h-screen grid grid-cols-4 gap-4  ">
@@ -39,17 +17,11 @@ const Profile = () => {
             <div class="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
             <div class="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
             <div class="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white dark:bg-gray-800">
-              {/* <img
+              <img
                 className="animate__animated animate__fadeInUp"
                 src="pokemon-full.jpg"
                 alt=""
-              ></img> */}
-              <SmartSlider
-                slides={slidesArray}
-                autoSlide={true}
-                height={570}
-                showIndicators={false}
-              />
+              ></img>
             </div>
           </div>
         </div>
